8ea10fbd5e736feed0c88273601f4092ccec1d2c
Чернышев Андрей <bollgade@yandex.ru>
2026-02-21 13:46:01 +0300

    feat: add imports

---
+++ src/configs/imports.ts
@@ -0,0 +1,73 @@
+import type { Linter } from 'eslint';
+import { flatConfigs as importPluginFlat } from 'eslint-plugin-import';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+
+import type { ImportsOptions } from '../types';
+
+export function importsConfigs(
+  options: ImportsOptions,
+  hasTypescript: boolean,
+): Linter.Config[] {
+  const { rules } = options;
+
+  const configs: Linter.Config[] = [
+    importPluginFlat.recommended,
+    {
+      plugins: {
+        'unused-imports': eslintPluginUnusedImports,
+      },
+      rules: {
+        'unused-imports/no-unused-imports': 'error',
+        'import/order': [
+          'error',
+          {
+            groups: [
+              ['builtin', 'external'],
+              'internal',
+              ['parent', 'sibling', 'index'],
+            ],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: [
+              '**/*.config.{ts,js}',
+              '**/eslint.config.*',
+              '**/*.{test,spec}.{ts,js}',
+              '**/{tests,__tests__,test,config}/**/*.{ts,js}',
+            ],
+          },
+        ],
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+      },
+    },
+  ];
+
+  if (hasTypescript) {
+    configs.push(
+      importPluginFlat.typescript,
+      {
+        files: ['**/*.{ts,tsx}'],
+        settings: {
+          'import/resolver': {
+            typescript: {
+              alwaysTryTypes: true,
+            },
+          },
+        },
+      },
+    );
+  }
+
+  if (rules) {
+    configs.push({ rules });
+  }
+
+  return configs;
+}
