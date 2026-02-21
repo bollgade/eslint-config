04e46f3544efbe24c05a8c88c80aab433d73ddb3
Чернышев Андрей <bollgade@yandex.ru>
2026-02-21 13:34:04 +0300

    feat: add config for prettier

---
+++ src/configs/prettier.ts
@@ -0,0 +1,23 @@
+import type { Linter } from 'eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+import type { PrettierOptions } from '../types';
+
+export function prettierConfigs(options: PrettierOptions): Linter.Config[] {
+  const { rules } = options;
+
+  const configs: Linter.Config[] = [
+    eslintPluginPrettierRecommended satisfies Linter.Config,
+    {
+      rules: {
+        'prettier/prettier': ['error', { singleQuote: true }],
+      },
+    },
+  ];
+
+  if (rules) {
+    configs.push({ rules });
+  }
+
+  return configs;
+}
